9a50d3ecafc6474cbba448ee82927ec03e4816b6
Sebastian <spinzonrojas@gmail.com>
2025-08-19 21:56:16 -0500

    segundo commit

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def home_page_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Welcome to the Home Page</h1>")
+import pathlib
+
+def home_page_view(request):
+    
+    my_title = "my page"
+    mycontext = {
+        "pageTitle": my_title,
+    }
+
+    html_template = "home.html"
+    return render(request, html_template, mycontext)
